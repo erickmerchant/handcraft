a6c5b16fbe3c3a9e051d0d476a1a75e66f14a1d1
erickmerchant <erickmerchant@gmail.com>
2025-04-20 22:32:48 -0400

    Add dom/command

---
+++ dom/command.js
@@ -0,0 +1,17 @@
+import {HandcraftEventTarget} from "../dom.js";
+
+export function command(events, handler, options = {}) {
+	events = events.split(/\s+/);
+
+	return this.on(
+		"command",
+		(e) => {
+			if (events.includes(e.command)) {
+				return handler.bind(this)(e);
+			}
+		},
+		options
+	);
+}
+
+HandcraftEventTarget.prototype.command = command;
